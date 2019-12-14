6e8b8b869723f93d468f78a8a365f0fea5d8f6b2
Runar <runar@tuta.io>
2019-12-14 17:32:25 +0100

    pagenation

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,7 +2,12 @@ import React, { useEffect } from "react";
 import Link from "./Link";
 import gql from "graphql-tag";
 import { useQuery } from "react-apollo";
-
+import {
+  useLocation,
+  useParams,
+  useHistory
+} from "react-router-dom";
+import { LINKS_PER_PAGE } from "../constants";
 const NEW_VOTES_SUBSCRIPTION = gql`
   subscription {
     newVote {
@@ -79,13 +84,19 @@ export const FEED_QUERY = gql`
   }
 `;
 
-const LinkList = () => {
+const LinkList = props => {
   const {
     loading,
     error,
     data,
     subscribeToMore
-  } = useQuery(FEED_QUERY);
+  } = useQuery(FEED_QUERY, {
+    variables: getQueryVariables()
+  });
+
+  const location = useLocation();
+  const params = useParams();
+  const history = useHistory();
 
   const subscribeToNewVotes = subscribeToMoreVotes => {
     subscribeToMoreVotes({
@@ -122,6 +133,44 @@ const LinkList = () => {
   if (error) return <div>Error</div>;
 
   const linksToRender = data.feed.links;
+
+  const getQueryVariables = () => {
+    const isNewPage = location.pathname.includes("new");
+    const page = parseInt(params.page, 10);
+
+    const skip = isNewPage
+      ? (page - 1) * LINKS_PER_PAGE
+      : 0;
+    const first = isNewPage ? LINKS_PER_PAGE : 100;
+    const orderBy = isNewPage ? "createdAt_DESC" : null;
+    return { first, skip, orderBy };
+  };
+
+  const getLinksToRender = data => {
+    const isNewPage = location.pathname.includes("new");
+    if (isNewPage) return data.feed.links;
+    const rankedLinks = data.feed.links.slice();
+    rankedLinks.sort(
+      (l1, l2) => l2.votes.length - l1.votes.length
+    );
+    return rankedLinks;
+  };
+
+  const nextPage = data => {
+    const page = parseInt(params.page, 10);
+    if (page <= data.feed.count / LINKS_PER_PAGE) {
+      const nextPage = page + 1;
+      history.push("/new/${nextPage}");
+    }
+  };
+
+  const previousPage = () => {
+    const page = parseInt(params.page, 10);
+    if (page > 1) {
+      const previousPage = page - 1;
+      history.push("/new/${previousPage}");
+    }
+  };
 
   return (
     <div>
